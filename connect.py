7d485e9b6e183451d6b95e47387dca912209cbbc
Marc Menem <marc.menem@m4x.org>
2010-03-04 02:19:48 +0100

    Class based iTunes client list

--- connect.py
+++ connect.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -i
 
 import select
 import socket
@@ -8,6 +8,8 @@ import pybonjour
 
 regtype  = '_touch-able._tcp'
 timeout  = 5
+itunesClients = {}
+
 
 class itunes:
     def __init__(self, interfaceIndex, serviceName, replyDomain):
@@ -15,10 +17,12 @@ class itunes:
         self.interfaceIndex = interfaceIndex
         self.serviceName = serviceName
         self.replyDomain = replyDomain
-        
+        self.isresolved = False
+        self.isqueried = False
+    
         print 'Service added; resolving', interfaceIndex, serviceName, replyDomain
         
-        resolve_sdRef = pybonjour.DNSServiceResolve(
+        self.resolve_sdRef = pybonjour.DNSServiceResolve(
                                 0,
                                 interfaceIndex,
                                 serviceName,
@@ -26,21 +30,29 @@ class itunes:
                                 replyDomain,
                                 resolve_callback)
     
-        self.resolve_sdRef = resolve_sdRef
+        
+    def sockets(self):
+        if not self.isresolved: return [self.resolve_sdRef]
+        if not self.isqueried: return [self.query_sdRef]
     
-    def resolved(self, hosttarget, fullname):
-        query_sdRef = pybonjour.DNSServiceQueryRecord(
+    def resolved(self, hosttarget, fullname, port):
+        self.isresolved = True
+        self.query_sdRef = pybonjour.DNSServiceQueryRecord(
                                         interfaceIndex = self.interfaceIndex,
                                         fullname = hosttarget,
                                         rrtype = pybonjour.kDNSServiceType_A,
                                         callBack = query_record_callback)
-        self.query_sdRef = query_sdRef
         self.fullname = fullname
-        
+        self.hosttarget = hosttarget
+        self.port = port
+        self.resolve_sdRef.close()
+        self.resolve_sdRef = None
         
     def queried(self, rdata):
+        self.isqueried = True
         self.ip = socket.inet_ntoa(rdata)
-        
+        self.query_sdRef.close()
+        self.query_sdRef = None
 
     def show(self):
         print 'Resolved service:'
@@ -49,31 +61,27 @@ class itunes:
         print '  port       =', self.port  
         print '  IP         =', self.ip  
 
-
-
-itunesClients = {}
-
-resolvers = []
-queriers  = []
-
+    def closeConnexions(self):
+        if self.resolve_sdRef: self.resolve_sdRef.close()
+        if self.query_sdRef: self.query_sdRef.close()
+        
 
 
 def query_record_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
                           rrtype, rrclass, rdata, ttl):
     if errorCode == pybonjour.kDNSServiceErr_NoError:
-        it = itunesClients[interfaceIndex]
+        #it = itunesClients[fullname.split(".")[0]]
+        it = itunesClients.values()[0]
+        print "FIXME"
         it.queried( rdata )
         it.show()
         
 
 def resolve_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
                      hosttarget, port, txtRecord):
-    if errorCode != pybonjour.kDNSServiceErr_NoError:
-        return
-
-    it = itunesClients[interfaceIndex]
-    it.resolved( hosttarget, fullname )
-    queriers.append(it.query_sdRef)
+    if errorCode == pybonjour.kDNSServiceErr_NoError:
+        it = itunesClients[fullname.split(".")[0]]
+        it.resolved( hosttarget, fullname, port )
 
 
 def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName,
@@ -81,47 +89,50 @@ def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName,
     if errorCode != pybonjour.kDNSServiceErr_NoError:
         return
 
-    if not (flags & pybonjour.kDNSServiceFlagsAdd):
+    if flags & pybonjour.kDNSServiceFlagsAdd:
+        it = itunes( interfaceIndex, serviceName, replyDomain )
+        itunesClients[ serviceName ] = it
+        resolve_sdRef = it.resolve_sdRef
+    else:
         print 'Service removed', interfaceIndex, serviceName, replyDomain
-        return
+        it= itunesClients[ serviceName ]
+        it.closeConnexions()
+        del itunesClients[serviceName]
 
-    it = itunes(  interfaceIndex, serviceName, replyDomain)
-    itunesClients[ interfaceIndex ] = it
-    resolve_sdRef = it.resolve_sdRef
-    resolvers.append(resolve_sdRef)
-
-
-
-if __name__ == "__main__":
-    browse_sdRef = pybonjour.DNSServiceBrowse(regtype = regtype,
-                                              callBack = browse_callback)
     
+def populateITunes():
+    browse_sdRef = pybonjour.DNSServiceBrowse(regtype = regtype, callBack = browse_callback)
     
     try:
         try:
             while True:
-                socks = [browse_sdRef]
-                socks.extend(queriers)
-                socks.extend(resolvers)
 
-                if len(socks) > 1: 
-                    timeout = 5
-                else:
-                    timeout = None
+                timeout = None
+                socks = [browse_sdRef]
+                for client in itunesClients.values():
+                    extra = client.sockets()
+                    if extra:
+                        socks.extend(extra)
+                        timeout = 5
+                
                 ready, w, e = select.select(socks, [], [], timeout)
                 
                 if len(ready) == 0:
                     print "Resolve timed out"
+                    #need to remove timed out clients
                 
                 for sock in ready:
                     pybonjour.DNSServiceProcessResult(sock)
-                    if sock in resolvers:
-                        resolvers.remove( sock )
-                    if sock in queriers:
-                        queriers.remove( sock )
-                
     
         except KeyboardInterrupt:
             pass
     finally:
         browse_sdRef.close()
+        for client in itunesClients.values(): client.closeConnexions()    
+    
+
+if __name__ == "__main__":
+    populateITunes()
+    
+    
+    
