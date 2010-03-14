f03076b6c79a4e2c18db6dad641f05273a1cd558
Marc Menem <marc.menem@m4x.org>
2010-03-14 21:38:11 +0100

    Added Avahi support in connect routines

--- connect.py
+++ connect.py
@@ -1,20 +1,183 @@
 #!/usr/bin/python -i
 
 import select, socket, sys, threading
-import pybonjour
 
+__macosx__ = sys.platform == 'darwin'
 
 regtype  = '_touch-able._tcp'
 timeout  = 5
 itunesClients = {}
 
 
+if __macosx__:
+    import pybonjour
+    
+    def txtRec( tr ):
+        tr = pybonjour.TXTRecord.parse(tr)
+        d = {}
+        for k in tr: 
+            d[ k[0] ] = k[1]
+        return d
+    
+    def query_record_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
+                              rrtype, rrclass, rdata, ttl):
+        if errorCode == pybonjour.kDNSServiceErr_NoError:
+            for it in itunesClients.values():
+                if it.query_sdRef == sdRef:
+                    it.queried( rdata )
+                    it.show()
+                    return
+            
+    
+    def resolve_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
+                         hosttarget, port, txtRecord):
+        if errorCode == pybonjour.kDNSServiceErr_NoError:
+            it = itunesClients[fullname.split(".")[0]]
+            it.resolved( hosttarget, fullname, port, txtRecord )
+    
+            it.query_sdRef = pybonjour.DNSServiceQueryRecord( interfaceIndex = self.interfaceIndex,
+                                fullname = hosttarget, rrtype = pybonjour.kDNSServiceType_A,
+                                callBack = query_record_callback)
+            
+    
+    def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName,
+                        regtype, replyDomain):
+        if errorCode != pybonjour.kDNSServiceErr_NoError:
+            return
+    
+        if flags & pybonjour.kDNSServiceFlagsAdd:
+            it = itunes( interfaceIndex, serviceName, replyDomain )
+            itunesClients[ serviceName ] = it
+    
+            resolve_sdRef = pybonjour.DNSServiceResolve( 0, interfaceIndex, serviceName,
+                                regtype, replyDomain, resolve_callback)
+    
+            it.resolver = resolve_sdRef
+            
+        else:
+            print 'Service removed', interfaceIndex, serviceName, replyDomain
+            it = itunesClients[ serviceName ]
+            it.closeConnexions()
+            del itunesClients[serviceName]
+    
+        
+    loop = True
 
-def txtRec( tr ):
-    d = {}
-    for k in tr: 
-        d[ k[0] ] = k[1]
-    return d
+    def populateITunes():
+        browse_sdRef = pybonjour.DNSServiceBrowse(regtype = regtype, callBack = browse_callback)
+        loop = True
+        
+        try:
+            try:
+                while loop:
+    
+                    timeout = None
+                    socks = [browse_sdRef]
+                    processing = []
+                    for client in itunesClients.values():
+                        extra = client.sockets()
+                        if extra:
+                            processing.append( client )
+                            socks.extend(extra)
+                            timeout = 5
+                    
+                    ready, w, e = select.select(socks, [], [], timeout)
+                    
+                    if len(ready) == 0:
+                        # Remove timed out clients
+                        for client in processing:
+                            client.closeConnexions()
+                            del itunesClients[client.serviceName]
+                    
+                    for sock in ready:
+                        pybonjour.DNSServiceProcessResult(sock)
+        
+            except KeyboardInterrupt:
+                pass
+        finally:
+            browse_sdRef.close()
+            for client in itunesClients.values(): client.closeConnexions()    
+    
+    
+    
+    class browse(threading.Thread):
+        def __init__(self):
+            super(browse, self).__init__()
+            self.setDaemon(True)
+    
+        def run(self):
+            populateITunes()
+
+
+
+else:
+    import dbus, gobject, avahi
+    from dbus import DBusException
+    from dbus.mainloop.glib import DBusGMainLoop
+
+
+    def txtRec( tr ):
+        print "FIXME"
+        return {}
+
+    
+    def service_resolved(*args):
+        print 'service resolved'
+        print 'name:', args[2]
+        print 'address:', args[7]
+        print 'port:', args[8]
+        print 'hostname:', args[5]
+        
+        print "args", args
+        
+        "FIXME"
+        txtRecord = None
+        
+        it = itunesClients[args[2].decode() ]
+        it.resolved( args[7].decode() , args[5].decode() , args[8].real , txtRecord )
+        it.ip = args[7].decode() 
+        
+    
+    def print_error(*args):
+        print 'error_handler'
+        print args[0]
+        
+        
+    def myhandler(interface, protocol, name, stype, domain, flags):
+        print "+++ Found service '%s' type '%s' domain '%s' " % (name, stype, domain)
+        it = itunes( interface.real, name.decode() , domain.decode()  )
+        itunesClients[ name.decode()  ] = it
+         
+        server.ResolveService(interface, protocol, name , stype, 
+                domain, avahi.PROTO_UNSPEC, dbus.UInt32(0), 
+                reply_handler=service_resolved, error_handler=print_error)
+
+    
+    
+    def myhandler_rem(interface, protocol, name, stype, domain, flags):
+        print "--- Removed service '%s' type '%s' domain '%s' \n" % (name, stype, domain)
+        it = itunesClients[ name.decode() ]
+        del itunesClients[name.decode() ]
+
+
+    loop = DBusGMainLoop()
+    
+    bus = dbus.SystemBus(mainloop=loop)
+    
+    server = dbus.Interface( bus.get_object(avahi.DBUS_NAME, '/'),
+            'org.freedesktop.Avahi.Server')
+    
+    sbrowser = dbus.Interface(bus.get_object(avahi.DBUS_NAME,
+            server.ServiceBrowserNew(avahi.IF_UNSPEC,
+                avahi.PROTO_UNSPEC, regtype, 'local', dbus.UInt32(0))),
+            avahi.DBUS_INTERFACE_SERVICE_BROWSER)
+    
+    sbrowser.connect_to_signal("ItemNew", myhandler)
+    sbrowser.connect_to_signal("ItemRemove", myhandler_rem)
+
+
+
+
 
 class itunes:
     def __init__(self, interfaceIndex, serviceName, replyDomain):
@@ -27,9 +190,7 @@ class itunes:
     
         print 'Service added; resolving', interfaceIndex, serviceName, replyDomain
         
-        self.resolve_sdRef = pybonjour.DNSServiceResolve( 0, interfaceIndex, serviceName,
-                                regtype, replyDomain, resolve_callback)
-    
+        
         
     def sockets(self):
         if not self.isresolved: return [self.resolve_sdRef]
@@ -37,24 +198,24 @@ class itunes:
     
     def resolved(self, hosttarget, fullname, port, txtRecord):
         self.isresolved = True
-        self.query_sdRef = pybonjour.DNSServiceQueryRecord( interfaceIndex = self.interfaceIndex,
-                                        fullname = hosttarget, rrtype = pybonjour.kDNSServiceType_A,
-                                        callBack = query_record_callback)
         self.fullname = fullname
         self.hosttarget = hosttarget
         self.port = port
-        self.txtRecord = txtRec( pybonjour.TXTRecord.parse(txtRecord) )
-        self.dbName = self.txtRecord['CtlN']
-        self.dbId = self.txtRecord['DbId']
+        self.txtRecord = txtRec( txtRecord )
+        if self.txtRecord.has_key('CtlN'):  self.dbName = self.txtRecord['CtlN']
+        if self.txtRecord.has_key('DbId'):  self.dbId = self.txtRecord['DbId']
         
-        self.resolve_sdRef.close()
-        self.resolve_sdRef = None
+        if __macosx__:
+            self.resolve_sdRef.close()
+            self.resolve_sdRef = None
         
     def queried(self, rdata):
         self.isqueried = True
         self.ip = socket.inet_ntoa(rdata)
-        self.query_sdRef.close()
-        self.query_sdRef = None
+
+        if __macosx__:
+            self.query_sdRef.close()
+            self.query_sdRef = None
 
     def show(self):
         print 'Resolved service:'
@@ -67,94 +228,17 @@ class itunes:
 
 
     def closeConnexions(self):
-        if self.resolve_sdRef: self.resolve_sdRef.close()
-        if self.query_sdRef: self.query_sdRef.close()
+        if __macosx__:
+            if self.resolve_sdRef: self.resolve_sdRef.close()
+            if self.query_sdRef: self.query_sdRef.close()
         
-
-
-def query_record_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
-                          rrtype, rrclass, rdata, ttl):
-    if errorCode == pybonjour.kDNSServiceErr_NoError:
-        for it in itunesClients.values():
-            if it.query_sdRef == sdRef:
-                it.queried( rdata )
-                it.show()
-                return
-        
-
-def resolve_callback(sdRef, flags, interfaceIndex, errorCode, fullname,
-                     hosttarget, port, txtRecord):
-    if errorCode == pybonjour.kDNSServiceErr_NoError:
-        it = itunesClients[fullname.split(".")[0]]
-        it.resolved( hosttarget, fullname, port, txtRecord )
-
-
-def browse_callback(sdRef, flags, interfaceIndex, errorCode, serviceName,
-                    regtype, replyDomain):
-    if errorCode != pybonjour.kDNSServiceErr_NoError:
-        return
-
-    if flags & pybonjour.kDNSServiceFlagsAdd:
-        it = itunes( interfaceIndex, serviceName, replyDomain )
-        itunesClients[ serviceName ] = it
-        resolve_sdRef = it.resolve_sdRef
-    else:
-        print 'Service removed', interfaceIndex, serviceName, replyDomain
-        it = itunesClients[ serviceName ]
-        it.closeConnexions()
-        del itunesClients[serviceName]
-
-loop = True
-    
-def populateITunes():
-    browse_sdRef = pybonjour.DNSServiceBrowse(regtype = regtype, callBack = browse_callback)
-    loop = True
-    
-    try:
-        try:
-            while loop:
-
-                timeout = None
-                socks = [browse_sdRef]
-                processing = []
-                for client in itunesClients.values():
-                    extra = client.sockets()
-                    if extra:
-                        processing.append( client )
-                        socks.extend(extra)
-                        timeout = 5
-                
-                ready, w, e = select.select(socks, [], [], timeout)
-                
-                if len(ready) == 0:
-                    # Remove timed out clients
-                    for client in processing:
-                        client.closeConnexions()
-                        del itunesClients[client.serviceName]
-                
-                for sock in ready:
-                    pybonjour.DNSServiceProcessResult(sock)
-    
-        except KeyboardInterrupt:
-            pass
-    finally:
-        browse_sdRef.close()
-        for client in itunesClients.values(): client.closeConnexions()    
-
-class browse(threading.Thread):
-    def __init__(self):
-        super(browse, self).__init__()
-        self.setDaemon(True)
-
-    def run(self):
-        populateITunes()
 
 
 if __name__ == "__main__":
-    browse().start()
-    
-    
-#    populateITunes()
-    
-    
-    
+
+    if __macosx__:
+        browse().start()
+    else:
+        loop2 = gobject.MainLoop()
+        loop2.run()        
+        
